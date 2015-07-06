b0edff39e136ded3624f858266f644518d58b4d3
Steven Sheldon <steven@sasheldon.com>
2015-07-06 03:34:07 -0700

    Set version mins to avoid weird issues.

--- build.rs
+++ build.rs
@@ -1,5 +1,26 @@
 extern crate gcc;
 
+use std::env;
+
 fn main() {
-    gcc::compile_library("libexception.a", &["extern/exception.m"]);
+    let mut config = gcc::Config::new();
+
+    let target = env::var("TARGET").unwrap();
+    if target.contains("ios") {
+        if target.contains("i386") ||
+                target.contains("i686") ||
+                target.contains("x86_64") {
+            // Simulator
+            config.flag("-mios-simulator-version-min=4.2");
+        } else {
+            // Device
+            config.flag("-miphoneos-version-min=4.2");
+        }
+    } else if target.contains("darwin") {
+        // OSX
+        config.flag("-mmacosx-version-min=10.3");
+    }
+
+    config.file("extern/exception.m");
+    config.compile("libexception.a");
 }
